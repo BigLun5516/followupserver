0cc3bac9f43880b2cc4ed65e42c06d11aab3fd59
BigLun5516 <15538262125@163.com>
2021-02-22 14:20:12 +0800

    心理咨询师持久层表名更正

--- src/main/java/com/epic/followup/repository/managementSys/PsychologicalConsultantRepository.java
+++ src/main/java/com/epic/followup/repository/managementSys/PsychologicalConsultantRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface PsychologicalConsultantRepository extends JpaRepository<PsychologicalConsultantModel, Integer> {
 
     // 根据心理咨询中心id查询心理咨询师数量
-    @Query(nativeQuery = true, value = "SELECT COUNT(mpc.consultant_id) FROM management_paychological_consultant mpc WHERE mpc.center_id = ?1")
+    @Query(nativeQuery = true, value = "SELECT COUNT(mpc.consultant_id) FROM management_psychological_consultant mpc WHERE mpc.center_id = ?1")
     Integer getConsultantNum(Integer centerId);
 
     // 根据中心id查询咨询师
