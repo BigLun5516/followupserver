21ba1f0d4c008395f45135627445816f26f451bf
BigLun5516 <15538262125@163.com>
2021-02-25 16:01:17 +0800

    修复查询所有中心时查询不到没有学校使用的中心

--- src/main/java/com/epic/followup/repository/managementSys/PsychologicalCenterRepository.java
+++ src/main/java/com/epic/followup/repository/managementSys/PsychologicalCenterRepository.java
@@ -18,13 +18,13 @@ public interface PsychologicalCenterRepository extends JpaRepository<Psychologic
      * 根据查询条件查询
      */
     @Query(value = "SELECT c.center_id, u.university_name, c.center_name, c.center_manager, c.tel, c.total_num, c.accept_num, c.status, c.create_time\n" +
-            "from management_university u, management_psychological_center c\n" +
-            "where u.center_id = c.center_id and (u.university_name = ?1 or ?1 = \"\") " +
+            "from management_psychological_center c left join management_university u on c.center_id = u.center_id \n" +
+            "where (u.university_name = ?1 or ?1 = \"\") " +
             "and (c.center_name like ?2 or ?2 = \"\") and (c.status = ?3 or ?3 = -1) " +
             "and (c.create_time >= ?4 or ?4 = \"\") and (c.create_time <= ?5 or ?5 = \"\")"
             , countQuery = "SELECT count(*) " +
-            "from management_university u, management_psychological_center c\n" +
-            "where u.center_id = c.center_id and (u.university_name = ?1 or ?1 = \"\") " +
+            "from management_psychological_center c left join management_university u on c.center_id = u.center_id \n" +
+            "where (u.university_name = ?1 or ?1 = \"\") " +
             "and (c.center_name like ?2 or ?2 = \"\") and (c.status = ?3 or ?3 = -1) " +
             "and (c.create_time >= ?4 or ?4 = \"\") and (c.create_time <= ?5 or ?5 = \"\")"
             , nativeQuery = true)
