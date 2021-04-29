4c0524514dec31a447505d4fba7f84480744629d
lvguoguang <lvguoguang@hust.edu.cn>
2021-04-29 16:29:03 +0800

    任务查看sql语句小bug

--- src/main/java/com/epic/followup/repository/managementSys/task/TaskRepository.java
+++ src/main/java/com/epic/followup/repository/managementSys/task/TaskRepository.java
@@ -13,7 +13,7 @@ public interface TaskRepository extends JpaRepository<TaskModel, Long> {
     @Query(nativeQuery = true, value = "SELECT a.task_id,a.title,a.content,a.grade,a.scale_id,a.create_time,b.university_name,c.scale_name ,co.college_name  " +
             "FROM `management_task` a LEFT JOIN management_university b on a.university_id=b.university_id " +
             "LEFT JOIN management_scale c ON a.scale_id=c.scale_id LEFT JOIN management_college co ON co.college_id=a.college_id " +
-            "WHERE (a.university_id= ?1 or ?1=-1) AND (a.college_id in ?2 or -1 in ?2)" )
+            "WHERE (a.university_id= ?1 or ?1=-1) AND (a.college_id in ?2 or -1 in ?2 or a.college_id=-1)" )
     List<Object> findListByUid(Integer uid,List<Integer> colleges);
 
 }
