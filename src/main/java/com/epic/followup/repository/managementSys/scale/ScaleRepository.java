f4b5be041c40bfb33c4de728259a502b8fd38ddc
BigLun5516 <15538262125@163.com>
2021-04-29 13:30:43 +0800

    解决量表管理查询bug

--- src/main/java/com/epic/followup/repository/managementSys/scale/ScaleRepository.java
+++ src/main/java/com/epic/followup/repository/managementSys/scale/ScaleRepository.java
@@ -14,14 +14,37 @@ public interface ScaleRepository extends JpaRepository<ScaleModel, Long> {
      */
 
     // 根据条件查询
-    @Query(value = "select s.scale_id, scale_name, s.isnz, f.id is null, s.school_name\n" +
-            "from management_scale s left join management_scale_forbidden f on s.scale_id = f.scale_id \n" +
-            "left join management_university u on s.school_name = u.university_name\n" +
-            "where s.scale_name like %?1% " +
+    @Query(value = "select s.scale_id, scale_name, s.isnz, s.scale_id not in \n" +
+            "                                    (\n" +
+            "                                        select f.scale_id\n" +
+            "                                        from management_scale_forbidden f left join management_university u on f.university_name = u.university_name\n" +
+            "                                        where u.university_id = ?5\n" +
+            "                                    ) as status, s.school_name\n" +
+            "from management_scale s\n" +
+            "where s.scale_name like %?1% \n" +
             "   and (s.school_name = ?2 or ?2 = '' or s.school_name = '' or s.school_name is null)\n" +
-            "   and (s.isnz = ?3 or ?3 = -1) " +
-            "   and ((0 = ?4 and f.id is not null ) or (1 = ?4 and  f.id is null) or ?4 = -1)\n" +
-            "   and (u.university_id = ?5 or ?5 = -1 or u.university_id is null) \n", nativeQuery = true)
+            "   and (s.isnz = ?3 or ?3 = -1)\n" +
+            "   and ((1 = ?4 and s.scale_id not in \n" +
+            "                                    (\n" +
+            "                                        select f.scale_id\n" +
+            "                                        from management_scale_forbidden f left join management_university u on f.university_name = u.university_name\n" +
+            "                                        where u.university_id = ?5\n" +
+            "                                    ))\n" +
+            "                or (0 = ?4 and s.scale_id in \n" +
+            "                                    (\n" +
+            "                                        select f.scale_id\n" +
+            "                                        from management_scale_forbidden f left join management_university u on f.university_name = u.university_name\n" +
+            "                                        where u.university_id = ?5\n" +
+            "                                    ))\n" +
+            "                or -1 = ?4)\n" +
+            "   and (?5 = -1 or s.school_name = \n" +
+            "                   (\n" +
+            "                       select un.university_name\n" +
+            "                       from management_university un\n" +
+            "                       where un.university_id = ?5\n" +
+            "                    )\n" +
+            "                or s.school_name = '' or s.school_name is null\n" +
+            "        )", nativeQuery = true)
     List<Object> findScaleByQuery(String scaleName, String schoolName, int isnz, int status, Integer userUniversityId);
 
     // 根据条Id查询
