85ba8adf9808905cfc4ffccbf2c862c676433599
lvguoguang <lvguoguang@hust.edu.cn>
2021-05-07 14:45:54 +0800

    查询学生完成/未完成任务的时候加入scale_id

--- src/main/java/com/epic/followup/repository/managementSys/task/TaskStatusRepository.java
+++ src/main/java/com/epic/followup/repository/managementSys/task/TaskStatusRepository.java
@@ -8,13 +8,13 @@ import java.util.List;
 
 public interface TaskStatusRepository extends JpaRepository<TaskFinishedModel, Long> {
     //查找学生已完成的
-    @Query(nativeQuery = true, value = "SELECT a.task_id,b.title,b.content,b.create_time,c.scale_name FROM " +
-            "`management_task_finished` a LEFT JOIN management_task b ON a.task_id=b.task_id LEFT JOIN management_scale " +
-            "c ON  b.scale_id=c.scale_id WHERE a.user_id=?1")
+    @Query(nativeQuery = true, value = "SELECT a.task_id,b.title,b.content,b.create_time,c.scale_name,c.scale_id " +
+            "FROM `management_task_finished` a LEFT JOIN management_task b ON a.task_id=b.task_id " +
+            "LEFT JOIN management_scale c ON  b.scale_id=c.scale_id WHERE a.user_id=?1")
     List<Object> getTask1(Long userId);
 
     //查找学生未完成的(根据所有的任务再排除完成表中的任务）
-    @Query(nativeQuery = true, value = "SELECT c.task_id,c.title,c.content,c.create_time,d.scale_name " +
+    @Query(nativeQuery = true, value = "SELECT c.task_id,c.title,c.content,c.create_time,d.scale_name,,d.scale_id " +
             "FROM management_task c LEFT JOIN management_scale d ON c.scale_id=d.scale_id " +
             "WHERE c.task_id in (SELECT a.task_id FROM management_task a " +
             "WHERE a.university_id=?1 AND a.grade=?2 And (a.college_id=-1 or a.college_id =?4)  " +
